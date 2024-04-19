fad03aa3d5ed4d896217e7bb7ffa8dbe8718a6d0
Akeem Amusat <aoamusat@gmail.com>
2024-04-19 12:27:25 +0100

    Go local module

---
+++ greetings/go.mod
@@ -0,0 +1,3 @@
+module learngo.io/greetings
+
+go 1.21.1
--- hello/hello.go
+++ hello/hello.go
@@ -3,11 +3,15 @@ package main
 import (
 	"fmt"
 
+	"learngo.io/greetings"
 	"rsc.io/quote"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
-	fmt.Printf("#QOTD: %s\n", quote.Opt())
+
+	var message = greetings.HelloGreeting("Akeem")
+	fmt.Println(message)
+
+	fmt.Printf("\n#QOTD: %s\n", quote.Go())
 	fmt.Printf("Session ID: %s\n", generateSessionID())
 }
