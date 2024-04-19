06143fc7eebc219ecf02bc27df2a6cb15e5286f0
Akeem Amusat <aoamusat@gmail.com>
2024-04-19 15:55:56 +0100

    using map for messages

--- hello/hello.go
+++ hello/hello.go
@@ -12,12 +12,19 @@ func main() {
 
 	log.SetPrefix("Error: ")
 
-	message, err := greetings.HelloGreeting("Akeem")
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Println(message)
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	for _, m := range messages {
+		fmt.Println(m)
+	}
 
 	fmt.Printf("\n#QOTD: %s\n", quote.Go())
 	fmt.Printf("Session ID: %s\n", generateSessionID())
---
+++ sinewave/go.mod
@@ -0,0 +1,3 @@
+module learngo.io/sinewave
+
+go 1.21.1
