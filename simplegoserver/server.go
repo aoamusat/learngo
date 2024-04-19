4bf16a3c6a0aea10db01c3aaad88c9027927631f
Akeem Amusat <aoamusat@gmail.com>
2024-04-19 15:23:25 +0100

    JSON response in go server

--- simplegoserver/server.go
+++ simplegoserver/server.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 )
+
+// User represents a user object.
+type User struct {
+	ID        int    `json:"id"`
+	UserAgent string `json:"userAgent"`
+	IPAddress string `json:"ipAddress"`
+}
 
 func main() {
 	log.SetPrefix("Server Error: ")
@@ -17,5 +26,20 @@ func main() {
 
 // handler echoes the Path component of the request URL r.
 func handler(response http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(response, "URL.Path = %q\n", request.URL.Path)
+	response.Header().Set("Content-Type", "application/json")
+	response.Header().Set("X-API-RateLimit-Limit", "3600")
+	responseData := make(map[string]any)
+	responseData["path"] = request.URL.Path
+	user := User{
+		IPAddress: request.RemoteAddr,
+		ID:        rand.Intn(10),
+		UserAgent: request.Header.Get("User-Agent"),
+	}
+	responseData["user"] = user
+	jsonData, err := json.Marshal(responseData)
+	if err != nil {
+		http.Error(response, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	response.Write(jsonData)
 }
