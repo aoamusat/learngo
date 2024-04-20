97538c65eca0aa479eb248b0b47f604ca726476c
Akeem Amusat <aoamusat@gmail.com>
2024-04-20 18:27:42 +0100

    Go arrays

--- arrays/go.mod
+++ arrays/go.mod
@@ -1,3 +1,5 @@
 module learngo.io/arrays
 
 go 1.21.1
+
+require syreclabs.com/go/faker v1.2.3 // indirect
--- arrays/main.go
+++ arrays/main.go
@@ -1,15 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"syreclabs.com/go/faker"
+)
+
+type User struct {
+	name         string
+	address      string
+	emailAddress string
+	phoneNumber  string
+}
 
 func main() {
-	var users [5]string
+	var users [5]User
+	matrice := [3][3]int{
+		{3, -1, 0},
+		{0, -1, 1},
+		{1, 7, 2},
+	}
 
-	users[4] = "John Smith"
+	fmt.Println("Printing a multidimensional array")
+
+	for rowIndex, row := range matrice {
+		for colIndex, val := range row {
+			fmt.Printf("[%d][%d] => (%d)   ", rowIndex, colIndex, val)
+		}
+		fmt.Println("\n")
+	}
+
+	for i := 0; i < len(users); i++ {
+		users[i] = User{
+			emailAddress: faker.Internet().Email(),
+			name:         faker.Name().FirstName() + " " + faker.Name().LastName(),
+			address:      faker.Address().City() + ", " + faker.Address().Country(),
+			phoneNumber:  faker.PhoneNumber().CellPhone(),
+		}
+	}
 
 	for i, user := range users {
-		if user != "" {
-			fmt.Printf("%d => %s\n\a", i+1, user)
+		if user.address != "" {
+			fmt.Printf("%d => %v\n\a", i+1, user.emailAddress)
 		} else {
 			continue
 		}
