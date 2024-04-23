b85297238eeeb73bd9b830cb54384d0e774596d7
Akeem Amusat <aoamusat@gmail.com>
2024-04-23 21:33:02 +0100

    Add GORM

--- firstgoa/client.go
+++ firstgoa/client.go
@@ -1,4 +1,4 @@
-package clients
+package firstgoa
 
 import (
 	"context"
@@ -19,20 +19,47 @@ func NewClient(logger *log.Logger) client.Service {
 }
 
 // Add implements add.
-func (s *clientsrvc) Add(ctx context.Context, p *client.AddPayload) (err error) {
-	s.logger.Print("client.add")
+func (s *clientsrvc) Add(ctx context.Context,
+	p *client.AddPayload) (err error) {
+	s.logger.Print("client.add started")
+	newClient := client.ClientManagement{
+		ClientID:   p.ClientID,
+		ClientName: p.ClientName,
+	}
+	err = CreateClient(&newClient)
+	if err != nil {
+		s.logger.Print("An error occurred...")
+		s.logger.Print(err)
+		return
+	}
+	s.logger.Print("client.add completed")
 	return
 }
 
 // Get implements get.
-func (s *clientsrvc) Get(ctx context.Context, p *client.GetPayload) (res *client.ClientManagement, err error) {
-	res = &client.ClientManagement{}
-	s.logger.Print("client.get")
-	return
+func (s *clientsrvc) Get(ctx context.Context,
+	p *client.GetPayload) (res *client.ClientManagement, err error) {
+	s.logger.Print("client.get started")
+	result, err := GetClient(p.ClientID)
+	if err != nil {
+		s.logger.Print("An error occurred...")
+		s.logger.Print(err)
+		return
+	}
+	s.logger.Print("client.get completed")
+	return &result, err
 }
 
 // Show implements show.
-func (s *clientsrvc) Show(ctx context.Context) (res client.ClientManagementCollection, err error) {
-	s.logger.Print("client.show")
+func (s *clientsrvc) Show(ctx context.Context) (res client.ClientManagementCollection,
+	err error) {
+	s.logger.Print("client.show started")
+	res, err = ListClients()
+	if err != nil {
+		s.logger.Print("An error occurred...")
+		s.logger.Print(err)
+		return
+	}
+	s.logger.Print("client.show completed")
 	return
 }
--- firstgoa/go.mod
+++ firstgoa/go.mod
@@ -5,13 +5,20 @@ go 1.21.1
 require goa.design/goa/v3 v3.16.1
 
 require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.0 // indirect
+)
+
+require (
 	github.com/AnatolyRugalev/goregen v0.1.0 // indirect
 	github.com/dimfeld/httppath v0.0.0-20170720192232-ee938bf73598 // indirect
 	github.com/go-chi/chi/v5 v5.0.12 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/jinzhu/gorm v1.9.16
 	github.com/manveru/faker v0.0.0-20171103152722-9fbc68a78c4d // indirect
 	github.com/sergi/go-diff v1.3.1 // indirect
+	goa.design/plugins/v3 v3.16.1
 	golang.org/x/mod v0.17.0 // indirect
 	golang.org/x/net v0.24.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
